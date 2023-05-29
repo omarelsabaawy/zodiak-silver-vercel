e92728e00e9d26c34ee1131215b6f89c5a89e348
Omar Elsabaawy <76621027+omarelsabaawy@users.noreply.github.com>
2023-05-29 22:18:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -59,6 +59,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(
     session({
         secret: 'my secret',
